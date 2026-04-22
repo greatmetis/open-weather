50dd32b809781047b05011033f032c7de5c8d94b
Metis Teng <greatmetis@gmail.com>
2026-04-23 08:14:46 +1000

    init, including tailwind and tanstack installed; add api.ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { getWeather } from "./api";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+function App() {
+  const {data} = useQuery({
+    queryKey:['weather'],
+    queryFn: ()=>getWeather({lat:50,lon:50})
+  })
+  return (
+    <>
+      <div className="bg-red-500">Test</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/api.ts
@@ -0,0 +1,21 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+type getWeatherProps = {
+  lat:number,
+  lon:number
+}
+
+export async function getWeather({lat,lon}:getWeatherProps){
+  if(!API_KEY){
+    console.error('VITE_API_KEY not found');
+    return;
+  }
+
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=${API_KEY}`)
+
+  // parse into json
+  const data = await res.json();
+  return data
+}
+
+
